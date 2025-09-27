e9ad4f2d7343159dd52cce1510707992aa5072ff
eeequartey <efuaessilifua1@gmail.com>
2025-09-27 21:55:27 +0000

    arithmetic operations function

---
+++ fns_and_dsa/main.py
@@ -0,0 +1,13 @@
+from arithmetic_operations import perform_operation
+
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    operation = input("Enter the operation(add, subtract, multipy, divide): ").strip().lower()
+
+    result = perform_operation(num1, num2, operation)
+    print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
